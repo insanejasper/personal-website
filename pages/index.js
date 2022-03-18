807a7accae2ef4e7949d6b469fa0b4a842e51228
Paras Praj <paras.praj@icloud.com>
2022-03-18 01:15:20 -0400

    update to tailwind and remove boilerplate

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,26 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Footer from '../components/footer'
+import Head from 'next/head';
+import Footer from '../components/footer';
 
 export default function Home() {
-  const USERNAME = 'paraspraj'
+  const YEAR = new Date().getFullYear();
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Paras Praj - Software Engineer</title>
-        <meta
-          name="description"
-          content="I enjoy building software and solving problems"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Paras Praj
-        </h1>
-
-        <p className={styles.description}>
-          I enjoy building software and solving problems.
-          <br />
-          More content coming soon.
-        </p>
-
-        <div className={styles.grid}>
-          <div className={styles.card}>
-            GitHub - {' '}
-            <a
-              href={`https://github.com/${USERNAME}`}
-              rel="noreferrer"
-              target="_blank"
-            >
-              @{USERNAME}
-            </a>
-          </div>
-          <div className={styles.card}>
-            LinkedIn - {' '}
-            <a
-              href={`https://linkedin.com/in/${USERNAME}`}
-              rel="noreferrer"
-              target="_blank"
-            >
-              @{USERNAME}
-            </a>
-          </div>
-          {/* <div className={styles.card}>
-            Twitter - {' '}
-            <a
-              href={`https://twitter.com/${USERNAME}`}
-              rel="noreferrer"
-              target="_blank"
-            >
-              @{USERNAME}
-            </a>
-          </div> */}
-        </div>
-      </main>
-      <Footer />
+    <div className="bg-white dark:bg-black">
+      <div className="container mx-auto px-8 py-20 h-screen">
+          <h1 className="text-slate-900 dark:text-white text-2xl mt-20">Paras Praj</h1>
+          <p className="text-slate-500 dark:text-slate-400 mt-20">
+            I enjoy building software and solving problems.
+          </p>
+          <p className="text-slate-500 dark:text-slate-400 mt-2">
+            More content coming soon.
+          </p>
+          <ul className="list-disc text-slate-500 dark:text-slate-400 mt-20 pl-5" >
+            <li><a href="https://github.com/paraspraj" rel="noreferrer" target="_blank">GitHub</a></li>
+            <li><a href="https://linkedin.com/in/paraspraj" rel="noreferrer" target="_blank">LinkedIn</a></li>
+          </ul>
+          <p className="text-slate-500 dark:text-slate-400 mt-10 text-sm">
+            &copy; {YEAR} Paras Praj
+          </p>
+      </div>
     </div>
-  )
+  );
 }
